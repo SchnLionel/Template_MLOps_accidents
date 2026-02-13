fccffef19e86525945366a92901b2e96832820e9
Ubuntu <ubuntu@ip-172-31-15-19.eu-west-1.compute.internal>
2026-02-13 13:51:14 +0000

    Added data processing functions (commented)

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,6 +7,9 @@ import logging
 from sklearn.model_selection import train_test_split
 from check_structure import check_existing_file, check_existing_folder
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from src.features.build_features import add_new_columns, modif_target_variable, replace_values, drop_columns, drop_nans
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=False), required=0)
@@ -84,29 +87,21 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     df = fusion2.merge(df_caract, on = 'Num_Acc', how="left")
 
     #--Adding new columns
-    df = df.merge(nb_victim, on = "Num_Acc", how = "inner")
-    df.rename({"count" :"nb_victim"},axis = 1, inplace = True) 
-    df = df.merge(nb_vehicules, on = "Num_Acc", how = "inner") 
-    df.rename({"count" :"nb_vehicules"},axis = 1, inplace = True)
+    df = add_new_columns(df, nb_victim, nb_vehicules)
 
     #--Modification of the target variable  : 1 : prioritary // 0 : non-prioritary
-    df['grav'].replace([2,3,4], [0,1,1], inplace=True)
+    # df = modif_target_variable(df)
 
 
     #--Replacing values -1 and 0 
-    col_to_replace0_na = [ "trajet", "catv", "motor"]
-    col_to_replace1_na = [ "trajet", "secu1", "catv", "obsm", "motor", "circ", "surf", "situ", "vma", "atm", "col"]
-    df[col_to_replace1_na] = df[col_to_replace1_na].replace(-1, np.nan)
-    df[col_to_replace0_na] = df[col_to_replace0_na].replace(0, np.nan)
+    # df = replace_values(df)
 
 
     #--Dropping columns 
-    list_to_drop = ['senc','larrout','actp', 'manv', 'choc', 'nbv', 'prof', 'plan', 'Num_Acc', 'id_vehicule', 'num_veh', 'pr', 'pr1','voie', 'trajet',"secu2", "secu3",'adr', 'v1', 'lartpc','occutc','v2','vosp','locp','etatp', 'infra', 'obs' ]
-    df.drop(list_to_drop, axis=1, inplace=True)
+    # df = drop_columns(df)
 
     #--Dropping lines with NaN values
-    col_to_drop_lines = ['catv', 'vma', 'secu1', 'obsm', 'atm']
-    df = df.dropna(subset = col_to_drop_lines, axis=0)
+    # df = drop_nans(df)
 
 
     target = df['grav']
