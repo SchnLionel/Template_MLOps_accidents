9a21f99d8cd660c348e3c4cf74c6a3238e0a9e70
Ubuntu <ubuntu@ip-172-31-15-19.eu-west-1.compute.internal>
2026-02-16 19:29:11 +0000

    Trained and evaluated a Random Forest Classifier with n_estimators = 200 and criterion = entropy

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,12 +14,12 @@ y_test = pd.read_csv('data/preprocessed/y_test.csv')
 y_train = np.ravel(y_train)
 y_test = np.ravel(y_test)
 
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
+rf_classifier = ensemble.RandomForestClassifier(n_estimators=200, criterion="entropy", n_jobs=-1)
 
 #--Train the model
 rf_classifier.fit(X_train, y_train)
 
 #--Save the trained model to a file
-model_filename = './src/models/trained_model.joblib'
+model_filename = './models/trained_model.joblib'
 joblib.dump(rf_classifier, model_filename)
 print("Model trained and saved successfully.")
